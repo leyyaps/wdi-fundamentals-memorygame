b213ea1cb4ff9a3315d59fc1561825ced60070d1
leyyaps <andrew.smith@ipsofactouk.com>
2016-06-19 15:00:52 +0100

    fixed css and added else if to main.js

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,11 @@ var cardOne = "queen"
 var cardTwo = "queen"
 var cardThree = "king"
 var cardFour = "king"
+
+
+if (cardTwo === cardFour) {
+	alert ("You found a match!");
+
+} else {
+	alert ("Sorry, try again.");
+}
